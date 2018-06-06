79ea4facccf08cc398b0045d81d692a6cdc803f3
oya0831 <kd1243444@st.kobedenshi.ac.jp>
2018-06-06 03:09:22 +0000

    fix

--- IoT/sw/0J01026-2.py
+++ IoT/sw/0J01026-2.py
@@ -5,6 +5,9 @@ import datetime
 SW1=7
 PIN=21
 cnt=0
+dt_now=datetime.datetime.now()
+now1=0
+now2=0
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SW1,GPIO.IN)
@@ -14,7 +17,6 @@ while 1:
   print(cnt)
   time.sleep(1)
   if GPIO.input(SW1)==True:
-     dt_now=datetime.datetime.now()
      now1=dt_now.microsecond
      print("SW=",GPIO.input(SW1))
      cnt+=1
@@ -25,7 +27,6 @@ while 1:
      else:
         GPIO.output(PIN,GPIO.LOW)
 
-   dt_now=datetime.datetime.now()
-   now2=dt_now.microsecond
-   t1=now1-now2
-   print("time=",t1)
+  now2=dt_now.microsecond
+  t1=now1-now2
+  print("time=",t1)
