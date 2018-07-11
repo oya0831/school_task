e04bb3b923ecac9d40d0b5e9efb91ea964d41c2c
oya0831 <kd1243444@st.kobedenshi.ac.jp>
2018-07-11 02:58:19 +0000

    added 0j01026-4.py

---
+++ IoT/segment/0J01026-4.py
@@ -0,0 +1,46 @@
+# coding: utf-8
+#
+# すべてのセグメントを1秒ごとに点灯、消灯する
+
+import RPi.GPIO as GPIO
+import time
+
+# 7セグメントLEDとGPIOポートの対応付け
+LED_A=25
+LED_B=24
+LED_C=16
+LED_D=21
+LED_E=20
+LED_F=8
+LED_G=7
+LED_DP=12
+
+# ON/OFFの定義
+ON=GPIO.LOW
+OFF=GPIO.HIGH
+
+# warning抑止
+GPIO.setwarnings(False)
+
+# モード設定
+GPIO.setmode(GPIO.BCM)
+
+# PINというリスト(配列)を用意してGPIOピン番号を入れる
+PIN=[LED_A,LED_B,LED_C,LED_D,LED_E,LED_F,LED_G,LED_DP]
+
+# セグメント構成情報をDATAにセットする
+DATA=[0xfc,0x60,0xda,0xf2,0x66,0xb6,0xbe,0xe0,0xfe,0xe6,0xee,0x3e,0x9c,0x7a,0x9e,0x8e,0x01]
+
+# PINのそれぞれの要素を出力に設定
+for i in PIN:
+    GPIO.setup(i,GPIO.OUT)
+
+while True:
+  for i in DATA:
+      for j in PIN:
+           if i & 0x80==0x80:
+              GPIO.output(j,ON)
+           else:
+              GPIO.output(j,OFF)
+           i<<=1
+      time.sleep(1)
